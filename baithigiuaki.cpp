a3d6e0e88673768508ac60555ae8b4077dee47a3
tranlethuytrang <46455677+tranlethuytrang@users.noreply.github.com>
2019-01-12 02:33:36 +0700

    Create baithigiuaki.cpp

---
+++ baithigiuaki.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double tv; //tienvon;
+	double tkv; //tienkivong;
+	double ls; //laisuat;
+	sn=0; //sonam
+	cout<<"nhap vao so tien von, tien ki vong, lai suat: ";
+	cin>>tv>>tkv>>ls;
+	while(tv>0;tkv>0;ls>0;tv<tkv)
+	{
+		tv=tv+(tv*(ls/100));
+		sn=sn++;
+	}
+	while(tkv=tv)
+	{
+	 cout<<"so nam can de dat duoc so tien ki vong la: "<<sn<<endl;
+	}
+	return 0;
+}
